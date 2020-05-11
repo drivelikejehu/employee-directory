7b544976d520129c3752bf1c700ef07a352760f8
John Kang <pollyfactor@hotmail.com>
2020-05-10 23:31:32 -0400

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Directory from "./containers/EmployeeDirectory"
+import Navbar from "./components/Shared/Navbar"
+import Home from "./containers/Home"
 
 function App() {
   return (
     <div>
-      <Directory />
+      <Router>
+        <Navbar />
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/directory" component={Directory}/>
+      </Router>
     </div>
   );
 }
